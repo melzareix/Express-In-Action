adf2a972602ed510ab0df1f5086a7795edc37f9e
Mohamed Elzarei <mohamed.elzarei@gmail.com>
2017-02-10 11:13:29 +0200

    feat: Chapter 03 Guestbook Project Final

--- Chapter 03/Guestbook/app.js
+++ Chapter 03/Guestbook/app.js
@@ -13,3 +13,34 @@ app.locals.entries = entries;
 
 app.use(logger('dev'));
 app.use(bodyParser.urlencoded({extended: 'false'}));
+
+app.get('/', function (req, res) {
+    res.render('index');
+});
+
+app.get('/add-entry', function (req, res) {
+    res.render('add-entry');
+});
+
+app.post('/add-entry', function (req, res) {
+    var title = req.body.title;
+    var body = req.body.body;
+    if (!(title && body)) {
+        res.status(400).send('Entries should have title and body.');
+        return;
+    }
+    entries.push({
+        title: title,
+        published: new Date(),
+        body: body
+    });
+    res.redirect('/');
+});
+
+app.use(function (req, res) {
+    res.status(404).render('404');
+});
+
+app.listen(3000, function () {
+    console.log('Listening at http://localhost:3000');
+});
