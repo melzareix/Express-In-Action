abb77be9b9b2c579d281c46bd2aebd378e9b49cd
Mohamed Elzarei <mohamed.elzarei@gmail.com>
2017-02-10 16:23:47 +0200

    fix: Replaced static middleware with express.static

--- Chapter 04/Static File Server/app.js
+++ Chapter 04/Static File Server/app.js
@@ -1,22 +1,11 @@
 var express = require('express');
 var path = require('path');
-var fs = require('fs');
 var logger = require('morgan');
 
 var app = express();
 
 app.use(logger('dev'));
-
-app.use(function (req, res, next) {
-    var filePath = path.join(__dirname, 'static', req.url);
-    fs.stat(filePath, function (err, fileInfo) {
-        if (err || !fileInfo.isFile()) {
-            next();
-            return;
-        }
-        res.sendFile(filePath);
-    });
-});
+app.use(express.static(path.join(__dirname, 'static')));
 
 app.use(function (req, res) {
     res.status(404).send('File Not Found!');
