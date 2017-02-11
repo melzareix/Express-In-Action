6555cc40fdebfab01ed5fd92dfaeeccf6eaf09af
Mohamed Elzarei <mohamed.elzarei@gmail.com>
2017-02-11 21:52:36 +0200

    feat: Chapter 08, app.js

--- Chapter 08/app.js
+++ Chapter 08/app.js
@@ -1,6 +1,31 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const flash = require('connect-flash');
+const session = require('express-session');
 
 var app = express();
+var routes = require('./routes');
+
+mongoose.connect('mongodb://localhost:27017/test');
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+app.use(cookieParser());
+app.use(session({
+    secret: "TKRv0IJs=HYqrvagQ#&!F!%V]Ww/4KiVs$s,<<MX",
+    resave: true,
+    saveUninitialized: true
+}));
+app.use(flash());
+
+app.use(routes);
 app.listen(3000, function () {
     console.log('Server working on : http://localhost:3000');
 });
